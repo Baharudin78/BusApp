ee64384b068c131ed48f260576f17244544afd0b
Baharudin78 <bencoxgame78@gmail.com>
2021-11-16 22:08:10 +0700

    Initial commit

---
+++ app/src/main/java/com/baharudin/busapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.baharudin.busapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
